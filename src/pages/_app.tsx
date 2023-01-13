1fe8779421a042a1848c73ed406af8663d618df8
Lucas de Souza Azara <lucas.s.azara@gmail.com>
2023-01-13 08:15:46 -0300

    Layout Creation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,22 @@
+// Props
 import type { AppProps } from 'next/app'
+// CSS
+import { globalStyles } from '../styles/global'
+import { Container, Header } from '../styles/pages/app'
+// Logo
+import logo from '../assets/logo.svg'
+// Next Image
+import Image from 'next/image'
+
+globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>  
+        <Image src={logo.src} alt="" width={logo.width} height={logo.height} />
+      </Header> 
+      <Component {...pageProps} />
+    </Container>
+  )
 }
